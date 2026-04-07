7b2a09d81837384912eb320adf107bb21fa91a74
Rohini D <44237192+rohini1504@users.noreply.github.com>
2026-04-07 14:00:18 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from .database import Base, engine
-from .routes import router
-
-Base.metadata.create_all(bind=engine)
-
-app = FastAPI(title="Task Manager API")
-
-app.include_router(router)
